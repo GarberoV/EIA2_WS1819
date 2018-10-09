ab7addcbd56ec76fd5991c1c6e338ecb437c499b
VeroMarin <VeroMarin@LAPTOP-E04TPUO0>
2018-10-09 16:51:38 +0200

    Miniseite

--- Miniseite/miniseite.js
+++
@@ -1,5 +0,0 @@
-function getName() {
-    var name = prompt("Geben Sie hier bitte ihren Namen ein:", " ");
-    document.write("Herzlich Willkommen, ", name);
-}
-//# sourceMappingURL=miniseite.js.map
