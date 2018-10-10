e8b98dc4fcd17825a0da14031ca82bcff3e2c437
VeroMarin <VeroMarin@LAPTOP-E04TPUO0.local>
2018-10-10 19:25:56 +0200

    Aufgabe 0, Miniseite

---
+++ Aufgabe0/Aufgabe0.js
@@ -0,0 +1,5 @@
+function getName() {
+    var name = prompt("Bitte gebe deinen Namen ein:"," ");
+    document.write("Herzlich Willkommen, ", name);
+}
+//# sourceMappingURL=aufgabe0.js.map
---
+++ Aufgabe0/Aufgabe0.ts
@@ -0,0 +1,21 @@
+/*
+Aufgabe: 0
+Name: Veronika Garbero
+Matrikel: 256581
+Datum: 07.10.2018
+
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+
+namespace aufgabe0 {
+
+function getName(){
+    
+    var name = prompt("Bitte gebe deinen Namen ein:"," ");
+        document.write("Herzlich Willkommen, " ,name);      
+}
+    document.addEventListener('DOMContentLoaded', getName);
+    
+}
