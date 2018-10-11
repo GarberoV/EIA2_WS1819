082d311f018798e71dd0f27224eb01f6d80f5426
VeroMarin <VeroMarin@LAPTOP-E04TPUO0.eduroam.hs-furtwangen.de>
2018-10-11 12:39:05 +0200

    Aufgabe 0

--- Aufgabe0/Aufgabe0.js
+++ Aufgabe0/Aufgabe0.js
@@ -1,5 +1,20 @@
-function getName() {
-    var name = prompt("Bitte gebe deinen Namen ein:"," ");
-    document.write("Herzlich Willkommen, ", name);
-}
-//# sourceMappingURL=aufgabe0.js.map
+/*
+Aufgabe: 0
+Name: Veronika Garbero
+Matrikel: 256581
+Datum: 07.10.2018
+
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+var aufgabe0;
+(function (aufgabe0) {
+    function getName() {
+        var name = prompt("Bitte gebe deinen Namen ein:", " ");
+        document.write("Herzlich Willkommen, ", name);
+        console.log("Herzlich Willkommen ", name);
+    }
+    document.addEventListener('DOMContentLoaded', getName);
+})(aufgabe0 || (aufgabe0 = {}));
+//# sourceMappingURL=Aufgabe0.js.map
