9daf48f96fa85f81aaf55eb7d98be6870f6e8263
GarberoV <veronika.garbero@hs-furtwangen.de>
2018-11-14 18:53:16 +0100

    Aufgabe 4 - FormElements

---
+++ Aufgabe4_FormElements/A10-Test2.ts
@@ -0,0 +1,15 @@
+/*
+Aufgabe: Aufgabe 10, FormElements: Weihnachtsbaumkonfigurator
+Name: Veronika Garbero
+Matrikel: 256581
+Datum: 30.12.2017
+Hiermit versichere ich, dass ich diesen
+Code selbst geschrieben habe. Er wurde
+nicht kopiert und auch nicht diktiert.
+*/
+
+
+
+namespace A10 {
+    
+    }
